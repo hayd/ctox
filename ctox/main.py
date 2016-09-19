6f032488ad67170d57d025a830d7b967075b0d7f
Andy Hayden <andyhayden1@gmail.com>
2016-09-18 20:24:56 -0700

    :icecream: version bump to 0.1.3

--- ctox/main.py
+++ ctox/main.py
@@ -9,7 +9,7 @@ from ctox.shell import check_output, CalledProcessError  # TODO remove?
 
 from ctox.shell import cprint
 
-__version__ = version = '0.1.2'
+__version__ = version = '0.1.3'
 
 SUPPORTED_ENVS = ('py26', 'py27', 'py33', 'py34', 'py35')
 
